9ecdf195d38470833e54514823cfe4797d0da357
toby hutchinson <tobyhutchinson@gmail.com>
2023-03-09 15:30:35 +0000

    Started class declaration implementation

--- Parser Files/parser.c
+++ Parser Files/parser.c
@@ -5,61 +5,107 @@
 #include "lexer.h"
 #include "parser.h"
 
+// Global token and parserinfo variable
+Token t;
+ParserInfo pi;
 
 // Function Prototypes
-ParserInfo error(char *msg, Token T);
-void classDecl();
-void memberDecl();
-void classVarDecl();
-void type();
-void subroutineDecl();
-void paramList();
-void subroutineBody();
-void stmt();
-void varDeclarStmt();
-void letStmt();
-void ifStmt();
-void whileStmt();
-void doStmt();
-void subroutineCall();
-void expressionList();
-void returnStmt();
-void expression();
-void relationalExpression();
-void arithmeticExpression();
-void term();
-void factor();
-void operand();
+ParserInfo error(char *msg);
+ParserInfo classDecl();
+ParserInfo memberDecl();
+ParserInfo classVarDecl();
+ParserInfo type();
+ParserInfo subroutineDecl();
+ParserInfo paramList();
+ParserInfo subroutineBody();
+ParserInfo stmt();
+ParserInfo varDeclarStmt();
+ParserInfo letStmt();
+ParserInfo ifStmt();
+ParserInfo whileStmt();
+ParserInfo doStmt();
+ParserInfo subroutineCall();
+ParserInfo expressionList();
+ParserInfo returnStmt();
+ParserInfo expression();
+ParserInfo relationalExpression();
+ParserInfo arithmeticExpression();
+ParserInfo term();
+ParserInfo factor();
+ParserInfo operand();
 
 
-int InitParser (char* file_name)
-{
+int InitParser (char* file_name) {
 	return InitLexer(file_name);
 }
 
-ParserInfo Parse ()
-{
-	ParserInfo pi;
-	Token t = GetNextToken();
 
-	// Expect class declaration
-	if ( !strcmp(t.lx, "class") && t.tp == RESWORD )
-		; // Be happy :) 
-
-
-	pi.er = none;
+ParserInfo Parse () {
+	pi = classDecl();
 	return pi;
 }
 
 
-int StopParser ()
-{
+ParserInfo classDecl() {
+
+	// Expect class keyword
+	t = GetNextToken();
+	if ( !strcmp(t.lx, "class") && t.tp == RESWORD )
+		; // Be happy :) 
+	else {
+		pi = error("keyword 'class' expected");
+		pi.er = classExpected;
+	}
+
+	// Expect class id 
+	t = GetNextToken();
+	if ( t.tp == ID )
+		; // We good
+	else {
+		pi = error("identifier expected at this position");
+		pi.er = idExpected;
+	}
+
+	// Expect Open brace
+	// **** TURN INTO ITS OWN FUNCTION? ****
+	t = GetNextToken();
+	if ( t.tp == SYMBOL && !strcmp(t.lx, "{"))
+		; // Excellent
+	else {
+		pi = error("'{' expected at this position.");
+		pi.er = openBraceExpected;
+	}
+
+	// Expect 0 or more member declarations
+	
+	// Expect closing brace
+	t = GetNextToken();
+	if ( t.tp == SYMBOL && !strcmp(t.lx, "} at this position"))
+		; // Excellent
+	else {
+		pi = error("'}' expected.");
+		pi.er = openBraceExpected;
+	}
+	
+	return pi;
+}
+
+
+int StopParser () {
 	return StopLexer();
 }
 
+
+ParserInfo error(char *msg) {
+	ParserInfo pi;
+	printf("Error, line %i, close to %s, %s.\n", t.ln, t.lx, msg);
+	pi.tk = t;
+	return pi;
+}
+
+
 #ifndef TEST_PARSER
-int main ()
-{
+int main () {
 
 	return 1;
 }
