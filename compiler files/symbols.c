79c8b68553dc8e250490e44a3525e819270768ae
toblaroni <tobyhutchinson@gmail.com>
2023-04-21 19:42:30 +0100

    Added various functions to help with adding symbols and searching for symbols

--- compiler files/symbols.c
+++ compiler files/symbols.c
@@ -26,6 +26,18 @@ const int MAX_METHODS = 128; // Maxiumum methods in a class
 // Is the global scope which contains the class level tables
 symbolTable progTable;
 
+// Inserting symbols into class level scope or method level scope
+static void insertToClass( symbol s );
+static void insertToMethod( symbol s );
+// Inserting a class or method into the current scope
+static void insertClass();
+static void insertMethod();
+static int findInClass( char *name );
+static int findInMethod( char *name );
+// Get the current method / class
+static symbolTable *getCurrentClass(); 
+static symbolTable *getCurrentMethod(); 
+
 // Initialise the program table.
 void initSymTable() {
 
@@ -45,6 +57,95 @@ void initSymTable() {
 	progTable.classCount  = 0;
 }
 
+
+void insertSymbol( symbol s ) {
+
+	// Check that the symbol doesn't already exist
+	// if ( findSymbol( s ) != -1 ) ...
+
+	if ( progTable.scope == CLASS_SCOPE )
+		insertToClass(s);
+	else if ( progTable.scope == METHOD_SCOPE )
+		insertToMethod(s);
+
+	// Check that the maximum number of symbols hasn't been reached
+	if ( progTable.symbolCount == MAX_SYMBOLS ) {
+		printf("ERROR: Maxiumum number of Program Level symbols reached. Identifier: %s\n", s.name);
+		exit(-1);
+	}  	
+
+	progTable.symbols[progTable.symbolCount] = s;
+	progTable.symbolCount ++;
+}
+
+// Loop thorugh the symbols and see if the name already exists
+// Return the index of the symbol if it exists
+// Else return -1
+int findSymbol( char *name ) {
+	
+	// First check current scope.
+	// Then check the scope above ( if method, then class etc. )
+	if ( progTable.scope == METHOD_SCOPE ) {
+		int symbolIndex = findInMethod(name);
+
+		if ( symbolIndex != -1 )
+			return symbolIndex;
+
+	} else if ( progTable.scope == CLASS_SCOPE ) {
+		int symbolIndex = findInClass(name);
+
+		if ( symbolIndex != -1 )
+			return symbolIndex;
+	} 
+
+	// Search Prog Level
+	for ( int i = 0; i < progTable.symbolCount; ++i ) {
+		symbol sym = progTable.symbols[i];
+		if (!strcmp(sym.name, name))
+			return i;
+	}
+	
+	return -1;
+}
+
+
+void closeTable() {
+}
+
+
+static int findInMethod( char *name ) {
+	symbolTable *currentMethod = getCurrentMethod();
+
+	// Search Prog Level
+	for ( int i = 0; i < currentMethod->symbolCount; ++i ) {
+		symbol sym = currentMethod->symbols[i];
+		if (!strcmp(sym.name, name))
+			return i;
+	}
+
+	// Search the class
+	int symbolIndex = findInClass( name );
+	if ( symbolIndex != -1 )
+		return symbolIndex;
+
+	return -1;
+}
+
+
+static int findInClass( char *name ) {
+	symbolTable *currentClass = getCurrentClass();
+
+	// Search Prog Level
+	for ( int i = 0; i < currentClass->symbolCount; ++i ) {
+		symbol sym = currentClass->symbols[i];
+		if (!strcmp(sym.name, name))
+			return i;
+	}
+	
+	return -1;
+}
+
+
 // Insert a symbol into the current class table
 static void insertToClass( symbol s ) {
 
@@ -61,46 +162,29 @@ static void insertToClass( symbol s ) {
 	currentClass->symbolCount++;
 }
 
+
+// Inserting a symbol into the current level scope
 static void insertToMethod( symbol s ) {
 
 	symbolTable *currentClass = (progTable.tables + (progTable.classCount-1));
 	symbolTable *currentMethod = (currentClass->tables + currentClass->methodCount-1);
 	
+	// Check that there's space
 	if ( currentMethod->symbolCount == MAX_SYMBOLS ) {
 		printf("ERROR: Maxiumum number of method level symbols reached. Identifier: %s\n", s.name);
 		exit(-1);
 	}
+
+	currentMethod->symbols[currentMethod->symbolCount] = s;
+	currentMethod->symbolCount ++;
 }
 
-void insertSymbol( symbol s ) {
 
-	// Check that the symbol doesn't already exist
-
-	if ( progTable.scope == CLASS_SCOPE )
-		insertToClass(s);
-	else if ( progTable.scope == METHOD_SCOPE )
-		insertToMethod(s);
-
-	// Check that the maximum number of symbols hasn't been reached
-	if ( progTable.symbolCount == MAX_SYMBOLS ) {
-		printf("ERROR: Maxiumum number of Program Level symbols reached. Identifier: %s\n", s.name);
-		exit(-1);
-	}  	
-
-
-	// Depending on what scope the table is currently at, append the table accordingly
-	if ( progTable.scope == PROG_SCOPE ) {
-		*(progTable.symbols + progTable.symbolCount) = s;
-
-	}
+static symbolTable *getCurrentClass() {
+	return (progTable.tables + (progTable.classCount-1));
 }
 
-// Loop thorugh the symbols and see if the name already exists
-// Return the index of the symbol if it exists
-// Else return -1
-int findSymbol() {
-	return 0;
-}
-
-void closeTable() {
+static symbolTable *getCurrentMethod() {
+	symbolTable *currentClass = getCurrentClass();
+	return (currentClass->tables + currentClass->methodCount-1);
 }
--- compiler files/symbols.h
+++ compiler files/symbols.h
@@ -9,13 +9,13 @@
 #define CLASS_SCOPE  1
 #define METHOD_SCOPE 2
 
-
 const int MAX_SYMBOLS = 256; // Maxumimum symbols in a table
 
 // define your own types and function prototypes for the symbol table(s) module below
 typedef enum { TYPE, VAR, FUNCTION, BAD_TYPE } Type;
 typedef enum { INTEGER, PARAM, CLASS, STR, ARRAY, VOID } Kind;
 typedef enum { STATIC, FIELD } VarType;
+
 
 // Structure that holds the attributes of 
 typedef struct {
@@ -24,8 +24,8 @@ typedef struct {
   Kind returnType;
   Kind arrayType;
   VarType varType;
-
 } attributes;
+
 
 typedef struct {
   char *name;
@@ -33,11 +33,11 @@ typedef struct {
   attributes *attr; // Attributes ( Pointer to further data about the symbol )
 } symbol;
 
+
 // Holds a list of symbols and other tables
 // Program table is the top level scope
 // Then class level
 // Then method level
-
 typedef struct symbolTable symbolTable;
 
 struct symbolTable {
@@ -53,7 +53,7 @@ struct symbolTable {
 void initTable();
 void insertSymbol( symbol s ); // Insert takes a symbol and inserts it into the table
 void insertTable(); // Insert a table into an existing table
-int findSymbol();
+int findSymbol( char *name );
 void closeTable();
 
 #endif
